555429ea0e5b8a99f2c85de7877b9984deb5485f
acidcondor <rbelloni_86@hotmail.com>
2013-07-18 05:14:03 +0000

    web.js

--- web.js
+++ web.js
@@ -4,7 +4,12 @@ var app = express.createServer(express.logger());
 
 var messdata;
 
-fs.readFileSync('index.html', function (err, data) {
+app.get('/', function(request, response) {
+ var k = fs.readFileSync('./index.html','utf8');
+ response.send( k );
+});
+
+/*fs.readFileSync('index.html', function (err, data) {
   if (err) throw err;
   messdata = data;
 });
@@ -12,7 +17,7 @@ fs.readFileSync('index.html', function (err, data) {
 app.get('/', function(request, response) {
   response.send( messdata.toString('utf8'));
 });
-
+*/
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
